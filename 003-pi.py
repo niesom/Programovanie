a215cefc5b6199dbef6e30618b047f9870df374f
niesom <85696892+niesom@users.noreply.github.com>
2023-09-14 16:38:56 +0200

    Add files via upload

---
+++ 003-pi.py
@@ -0,0 +1,12 @@
+from math import sqrt
+pi = 3.141592653589793
+first_value = 223/71
+second_value = 22/17 + 37/47 + 88/83
+third_value = 99**2/(2206*sqrt(2))
+fourth_value = sqrt(sqrt(sqrt(5)+6)+7)
+fifth_value = (10**100/11222.11122)**(1/193)
+print(first_value-pi)
+print(second_value-pi)
+print(third_value-pi)
+print(fourth_value-pi)
+print(fifth_value-pi)
---
+++ 004-magicke_cislo.py
@@ -0,0 +1,22 @@
+import random
+month = random.randint(1,12)
+year = random.randint(100,2022)
+if month in [1,3,5,7,8,10,12]:
+    day = random.randint(1,31)
+elif month in [4,6,9,11]:
+    day = random.randint(1,30)
+else:
+    if year%400==0 or (year%4==0 and year%100!=0):
+        day = random.randint(1,29)
+    else:
+        day = random.randint(1,28)
+def cipher_sum(n):
+    result = 0
+    while n > 0:
+        result += n%10
+        n = n//10
+    return result
+if cipher_sum(day) + cipher_sum(month) + cipher_sum(year) < 10:
+    print(day,month,year,"is magic number")
+else:
+    print(day,month,year,"is not magic number")
